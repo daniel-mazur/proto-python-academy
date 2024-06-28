d4219c252514f0639984bc06967fa276bd517233
Daniel Mazur <30695129+daniel-mazur@users.noreply.github.com>
2024-06-28 14:21:22 +0200

    Add files via upload

---
+++ lesson01/lekce01a.py
@@ -0,0 +1,7 @@
+# Cvičení 1. Rozdělení stringu
+print("Prvních 5 písmen:")
+print("indexování"[0:5])
+print("Posledních 5 písmen:")
+print("indexování"[::-1][0:5]) #správně(ji): print("indexování"[-5:0])
+print("Každé 3. písmeno (počínaje prvním) od 'i':")
+print("indexování"[::3])
