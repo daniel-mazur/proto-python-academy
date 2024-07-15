83778da25deefb78a14bc81c83e0ab7df5417f31
Daniel Mazur <30695129+daniel-mazur@users.noreply.github.com>
2024-07-15 15:18:19 +0200

    Add files via upload
    Main changes:
    - comments in English
    - lines cut down to <80 characters
    - cured spaces around operators
    - password not revealed while typing it (getpass.getpass())
    - program termination invoked directly (sys.exit()) instead of obscure loop breaks
    - text stats report (except histogram) condensed into one function call from main program

--- projekt-1/projekt_1.py
+++ projekt-1/projekt_1.py
@@ -1,86 +1,70 @@
 #!/usr/bin/python
 
 """
-projekt_1.py:
+projekt_1.py: Textový analyzátor
 
 autor:    Daniel Mazur
 e-mail:   daniel.mazur.cz@gmail.com
 discord:  daniel_67828
 
 """
-import task_template
-# print(len(task_template.TEXTS))
-from projekt_1_stat_funcs import wcountLowercase, wcountUppercase
-from projekt_1_stat_funcs import wcountNumeric, wsum, wcountNames
-from projekt_1_stat_funcs import wsumNumeric, getCharcountHistogram
+from getpass import getpass # to input password without displaying it
+from sys import exit  # to use sys.exit() for program termination
 
-#projekt_1_stat_funcs.wsum
+from task_template import TEXTS
+from projekt_1_stat_funcs import *
 
-# blok autorizace přístupu
-iddb = {
+# access authorization section
+iddb = {    # username:password database as a dictionary
     "bob": "123",
     "ann": "pass123",
     "mike": "password123",
     "liz": "pass123"
 }
 
-while True:
-    mid = input("Zadej uživatelské jméno> ")
-    mpw = input("Zadej heslo> ")
-    if mpw == iddb.get(mid):
-        break
-    else:
-        print("....Nesprávné už. jméno nebo heslo.")
-#end-while
+mid = input("username:")
+mpw = getpass("password:")
 
-print("Vítej v programu Textový analyzátor!")
-print("")
-print("Báze TEXTS obsahuje "+str(len(task_template.TEXTS))+" texty.")
-print("Vybírejte mezi nimi indexy, tj. 0 až "+str(len(task_template.TEXTS)-1)+".")
-print("Pozor: Výběrem jiné hodnoty program skončí.")
-print("")
+if not mid in iddb.keys():
+    print("unregistered user, terminating the program..")
+    exit()
+#endif
 
-num = 0
+if not mpw == iddb.get(mid):
+    print("password does not match username, terminating the program..")
+    exit()
+#endif
 
-while True:
-    # výběr textu pro analýzu
-    ind = input("Vyberte text ze seznamu>  ")
-    if not(ind.isnumeric()):
-        print("")
-        print("... Vybraná jiná než číselná hodnota, program teď skončí.")
+print('-' * 40)        
+print("Welcome to the app, " + mid)
+ntexts = len(TEXTS)
+print("We have " + str(ntexts) + " texts to be analyzed.")
+print('-' * 40)
 
-        break
-    elif not int(ind) in range(0,len(task_template.TEXTS)):        
-        print("")
-        print("... Vybraná číselná hodnota je mimo rozsah, program teď skončí.")
-        break
-    else:
-        # text vybraný, můžeme s ním pracovat
-        wtx = task_template.TEXTS[int(ind)]
-        print("*" * 30)
-        print("Vybrán text:")
-        print("*" * 30)
-        print(wtx)
-        print("*" * 30)
-        # máme na výběr z několika postupů:
-        #    https://www.geeksforgeeks.org/iterate-over-words-of-a-string-in-python/
-        # začneme tím, který NEpotřebuje regex
-        wlist = wtx.split()
-        print("")
-        print("TEXT STATS:")
-        print("")
-        print("1. Počet slov: ".ljust(48,' ')+str(len(wlist)).rjust(6,' ')+" *")
-        print("2. Počet slov začínajících Velkým písmenem: ".ljust(48,' ')+str(wcountNames(wlist)).rjust(6,' ')+" *")
-        print("3. Počet slov psaných VELKÝM písmem: ".ljust(48,' ')+str(wcountUppercase(wlist)).rjust(6,' ')+" *")
-        print("4. Počet slov psaných malým písmem: ".ljust(48,' ')+str(wcountLowercase(wlist)).rjust(6,' ')+" *")
-        print("5. Počet čísel (ne cifer): ".ljust(48,' ')+str(wcountNumeric(wlist)).rjust(6,' ')+" *")
-        print("6. Suma všech čísel (ne cifer) v textu: ".ljust(48,' ')+str(int(wsumNumeric())).rjust(6,' ')+" *")
-        print("*" * 56)
-        print("Histogram délek slov v textu:")
-        print("")
-        print(getCharcountHistogram(wlist))
+ind = 0
+prompt = "Enter a number btw. 1 and " + str(ntexts) + " to select: "
+while ind in range(ntexts): 
+    # terminates program if value is outside
+    # the number of texts in TEXTS list 
+    ind = int(input(prompt)) - 1
+    print('-' * 40)  
+    if not ind in range(ntexts):
+        print("number not in range, terminating the program..")
+        exit()
+    #endif
+ 
+    wtx = TEXTS[ind]   # valid text selected
 
-#end-while
+    # there are several ways to do this:
+    #    https://www.geeksforgeeks.org/iterate-over-words-of-a-string-in-python/
+    # we use one, which does NOT need regex
+    wlist = wtx.split()
+
+    print(getTextStatString(wlist))
+
+    print(getCharcountHistogram(wlist))
+
+# endwhile
 
 
 
--- projekt-1/projekt_1_stat_funcs.py
+++ projekt-1/projekt_1_stat_funcs.py
@@ -8,6 +8,8 @@ e-mail:   daniel.mazur.cz@gmail.com
 discord:  daniel_67828
 
 """
+# module offers functions that produce various statistics of given texts
+# that had already been transformed to lists of words (eg. using txt.split())
 
 def wcountLowercase(wlist):
     # counts lowercase words in word list 'wlist'
@@ -31,10 +33,11 @@ def wcountUppercase(wlist):
     return c
 # enddef
 
-wsum = 0.0
+wsum = 0.0  # global, to be exported only therough wsumNumeric() func below
 
 def wcountNumeric(wlist):
-    # counts numeric words (i.e. numbers) in word list 'wlist' and sums them up in a global var
+    # counts numeric words (i.e. numbers) in word list 'wlist' and sums them
+    # up in a global variable wsum
     global wsum
     wsum = 0.0   # nutné resetovat před každou další kompletní analýzou
     c = 0
@@ -48,7 +51,8 @@ def wcountNumeric(wlist):
 # enddef
 
 def wcountNames(wlist):
-    # counts names (i.e. words with capital first letter and NOT all-caps) in word list 'wlist'
+    # counts names (i.e. titlecase words, or words with capital first
+    # letter and NOT all-caps) in word list 'wlist'
     c = 0
     for w in wlist: 
         if w.isalpha() and w[0].isupper():
@@ -59,45 +63,62 @@ def wcountNames(wlist):
 # enddef
 
 def wsumNumeric():
+    # gives the value in wsum global, which 
+    # had been filled by wcountNumeric()
     global wsum
-    return wsum # funguje dobře, pokud je předtím zavolaná fce wcountNumeric()
+    return wsum # works only if preceded by wcountNumeric() on the same text
+# enddef
+
+def getTextStatString(wlist):
+    # calls text stat functions and assembles the output (multiline) string
+    sout = str()
+    sout = "There are " + str(len(wlist)) + " words in the selected text.\n"
+    sout += "There are " + str(wcountNames(wlist)) + " titlecase words.\n"
+    sout += "There are " + str(wcountUppercase(wlist)) + " uppercase words.\n"
+    sout += "There are " + str(wcountLowercase(wlist)) + " lowercase words.\n"
+    sout += "There are " + str(wcountNumeric(wlist)) + " numeric strings.\n"
+    sout += "The sum of all the numbers " + str(int(wsumNumeric())) + "\n"   
+
+    return sout
 # enddef
 
 def getCharcountHistogram(wlist):
-    # counts characters in every word in word list 'wlist', fills histogram list of int and
-    # transforms this list into multiline str sout, eg.
+    # counts characters in every word in word list 'wlist', fills histogram
+    # list of int and transforms this list into multiline str sout, eg.
     # '''
-    # 7| * 1
-    # 8| *********** 11
-    # 9| *************** 15
-    # 10| ********* 9
-    # 11| ********** 10
+    #  7| *                 |1
+    #  8| ***********       |11
+    #  9| ***************   |15
+    # 10| *********         |9
+    # 11| **********        |10
     # '''
 
     c = 0
-    #longest = 0
+
     lhist = []
     for w in wlist: 
         
         if len(lhist) <= len(w):
-            for ii in range(len(lhist),len(w)+1): # prodlužujeme histogram, aby se vešel zápočet aktuálně nejdelšího slova
+            for ii in range(len(lhist),len(w)+1): # extending histogram,
+                # so it can contain the currently-longest word count
                 lhist.append(0)
-            #endfor
+            # endfor
         # endif
         lhist[len(w)] += 1
     # endfor
 
-    # najdi nejvyšší hodnotu v histogramu, potřebujeme ji pro graf. alignment
+    # here to identify the biggest histogram value, needed for
+    # output alignment
     hmax = 0
     for x in lhist:
         if x > hmax:
             hmax = x
         # endif
     # endfor
-    # hmax obsahuje nejvyšší hodnotu histogramu
+    # hmax now conatins the biggest histogram value
     if hmax <= 8:
         just = 10
-    elif (hmax % 2) > 0:    # zarovnání just chci mít sudou hodnotu
+    elif (hmax % 2) > 0:    # padded size just to be an odd number
         just = hmax + 3
     else:
         just = hmax + 2
@@ -111,25 +132,23 @@ def getCharcountHistogram(wlist):
             sout += str(c).rjust(3,' ')+'|'
             stars = ""
             if x > 0:
-                stars = ('*' * x) # nevím, jestli to funguje pro x==0, proto ten 'if'
+                stars = ('*' * x) 
             #endif
             sout += stars.ljust(just,' ')+'|'+str(x) 
         else:
-            if x > 0: # toto je pro první nenulovou položku histogramu
+            if x > 0: # this is for the first (lowest-bin) non-zero
+                # histogram value
                 trig = True
                 sout = str(c).rjust(3,' ')+'|'
                 stars = ('*' * x)
                 sout += stars.ljust(just,' ')+'|'+str(x)
             # endif
-        #endif
+        # endif
         c += 1
         sout += '\n'
-        #if c < len(lhist):
-        #    sout += '\n'
-        #endif
     # endfor
     
-    # sout ještě třeba doplnit 'hlavičkou tabulky' ve tvaru
+    # building 'table header' for sout
     #----------------------------------------
     #LEN| OCCURENCES |NR.
     #----------------------------------------
@@ -139,7 +158,8 @@ def getCharcountHistogram(wlist):
         hdr += "LEN|OCCURENCES|NR."
     else:
         hdr += "LEN|"
-        occ = "OCCURENCES".rjust(int((just+10)/2),' ').ljust(int(just),' ') # symetrické odsazení, když len("OCCURENCES")==10
+        occ = "OCCURENCES".rjust(int((just+10)/2),' ').ljust(int(just),' ')
+        # eg. symmetric padding, len("OCCURENCES")==10
         hdr += occ
         hdr += "|NR.\n"
     hdr += "-" * 40
@@ -151,30 +171,6 @@ def getCharcountHistogram(wlist):
     return sout
 # enddef
 
-# t = 'modul s funkcemi pro zpracování textu v projekt_1.py '
-# wl = t.split()
-# print(*wl, sep= '\n')
-
-#print(str(wcountLowercase(wl)))
-
-# c = 0
-# for w in wl: 
-#     if w.islower():
-#         c += 1
-#     # endif
-# # endfor
-
-# print(c)
-# print(str(wcountLowercase(wl)))
 
 
-
-
-
-
-
-
-
-
-
-#end
+# endmodule
